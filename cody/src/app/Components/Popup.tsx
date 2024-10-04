19ab84ef514d5d057d451a34e365c0e7bcd659e9
unknown <hydra2medusa@gmail.com>
2024-10-04 18:48:12 +0800

    editing

--- cody/src/app/Components/Popup.tsx
+++ cody/src/app/Components/Popup.tsx
@@ -3,7 +3,9 @@ import opportunity_list from "./list/opportunity_list";
 const Popup = function () {
   return (
     <section className="z-10 bg-transparent w-[90%] h-[90%] rounded-lg">
-      <div>Title</div>
+      <div className="my-4">
+        <span className="font-bold text-3xl">Шийдлүүд</span>
+      </div>
       <div className="w-full p-8 overflow-scroll">
         <div></div>
       </div>
