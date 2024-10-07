bff0cfad551ebc92067e1ebbe621cfd1b9c4ce98
unknown <hydra2medusa@gmail.com>
2024-10-07 14:45:31 +0800

    'edited'

--- cody/src/app/Components/pages/SixthSection.tsx
+++ cody/src/app/Components/pages/SixthSection.tsx
@@ -1,7 +1,12 @@
 const SixthSection = function () {
   return (
-    <section>
-      <div>{""}</div>
+    <section className="h-[600px] py-12">
+      <div className="flex justify-center">
+        <span className="font-bold text-2xl">
+          Кодиг ашиглан Та юу бүтээж чадах вэ?
+        </span>
+      </div>
+      <div></div>
     </section>
   );
 };
