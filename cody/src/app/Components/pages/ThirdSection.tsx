b85d9d318d9a9086e68b1623925d5c02e8b06453
unknown <hydra2medusa@gmail.com>
2024-10-07 22:32:03 +0800

    'edited'

--- cody/src/app/Components/pages/ThirdSection.tsx
+++ cody/src/app/Components/pages/ThirdSection.tsx
@@ -10,7 +10,7 @@ const ThirdSection = function () {
   const [tap, setTap] = useState(0);
   return (
     <section className="h-[650px] overflow-hidden md:h-[480px]">
-      {tap == 0 ? (
+      {tap == 0 && (
         <div className="block my-4 px-10 transition md:flex">
           <Image src={slide_1} width={500} height={500} alt="" />
           <div className="px-10">
@@ -55,8 +55,8 @@ const ThirdSection = function () {
             </div>
           </div>
         </div>
-      ) : null}
-      {tap == 1 ? (
+      )}
+      {tap == 1 && (
         <div className="block my-4 px-10 transition md:flex">
           <Image src={slide_2} width={530} height={530} alt="" />
           <div className="px-10">
@@ -103,53 +103,55 @@ const ThirdSection = function () {
             </div>
           </div>
         </div>
-      ) : null}
-      <div className="block my-4 px-10 transition md:flex">
-        <Image src={slide_3} width={530} height={530} alt="" />
-        <div className="px-10">
-          <div className="mt-10 text-center md:text-left">
-            <span className="font-bold text-[20px]">
-              ПЛАТФОРМЫН ХҮЧИЙГ МЭДЭР
-            </span>
-          </div>
-          <div className="mt-4">
-            <span>
-              Дэлхийн тэргүүлэгч технологиудыг хослуулан ашиглаж, технологийн
-              тасралтгүй хөгжүүлэлтээр хэрэглэгч байгууллагыг хангана.
-              Платформын гайхалтай шийдлүүдийг ашиглан борлуулалтаа өсгөөрэй.
-            </span>
-          </div>
-          <div className="flex">
-            <Button
-              onClick={() => {
-                setTap(0);
-              }}
-              variant="contained"
-              className="rounded-full mx-2 bg-purple-600"
-            >
-              {""}
-            </Button>
-            <Button
-              onClick={() => {
-                setTap(1);
-              }}
-              variant="contained"
-              className="rounded-full mx-2 bg-purple-600"
-            >
-              {""}
-            </Button>
-            <Button
-              onClick={() => {
-                setTap(2);
-              }}
-              variant="contained"
-              className="rounded-full mx-2 bg-purple-600"
-            >
-              {""}
-            </Button>
+      )}
+      {tap == 2 && (
+        <div className="block my-4 px-10 transition md:flex">
+          <Image src={slide_3} width={530} height={530} alt="" />
+          <div className="px-10">
+            <div className="mt-10 text-center md:text-left">
+              <span className="font-bold text-[20px]">
+                ПЛАТФОРМЫН ХҮЧИЙГ МЭДЭР
+              </span>
+            </div>
+            <div className="mt-4">
+              <span>
+                Дэлхийн тэргүүлэгч технологиудыг хослуулан ашиглаж, технологийн
+                тасралтгүй хөгжүүлэлтээр хэрэглэгч байгууллагыг хангана.
+                Платформын гайхалтай шийдлүүдийг ашиглан борлуулалтаа өсгөөрэй.
+              </span>
+            </div>
+            <div className="flex">
+              <Button
+                onClick={() => {
+                  setTap(0);
+                }}
+                variant="contained"
+                className="rounded-full mx-2 bg-purple-600"
+              >
+                {""}
+              </Button>
+              <Button
+                onClick={() => {
+                  setTap(1);
+                }}
+                variant="contained"
+                className="rounded-full mx-2 bg-purple-600"
+              >
+                {""}
+              </Button>
+              <Button
+                onClick={() => {
+                  setTap(2);
+                }}
+                variant="contained"
+                className="rounded-full mx-2 bg-purple-600"
+              >
+                {""}
+              </Button>
+            </div>
           </div>
         </div>
-      </div>
+      )}
     </section>
   );
 };
