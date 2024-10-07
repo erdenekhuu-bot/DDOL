8260b75b97b209ca137494f4d1f93c1798299d70
unknown <hydra2medusa@gmail.com>
2024-10-07 14:31:05 +0800

    'edited'

--- cody/src/app/Components/Footer.tsx
+++ cody/src/app/Components/Footer.tsx
@@ -1,9 +1,38 @@
 const Footer = function () {
   return (
     <footer className="w-full flex justify-around bg-purple-900 text-white font-bold">
-      <div>КОДИ ПЛАТФОРМ</div>
-      <div>БИДНИЙ ТУХАЙ</div>
-      <div>ХОЛБОО БАРИХ</div>
+      <section className="text-white">
+        <div className="font-bold my-8">
+          КОДИ ПЛАТФОРМ
+          <div className="font-medium my-2">Платформын тухай</div>
+          <div className="font-medium my-2">Шийдэл</div>
+          <div className="font-medium my-2">Онцлог, боломжууд</div>
+          <div className="font-medium my-2">Бизнесийн төрөл</div>
+          <div className="font-medium my-2">Харилцагчид</div>
+          <div className="font-medium my-2">Web Submit 2022</div>
+        </div>
+      </section>
+      <section className="text-white">
+        <div className="font-bold my-8">
+          КОДИ ПЛАТФОРМ
+          <div className="font-medium my-2">Танилцуулга</div>
+          <div className="font-medium my-2">Үйлчилгээний төрөл</div>
+          <div className="font-medium my-2">Хамтрагч байгууллага</div>
+          <div className="font-medium my-2">Холбоо барих</div>
+          <div className="font-medium my-2">Ажлын байр</div>
+          <div className="font-medium my-2">Веб үйлчилгээний сургалт</div>
+        </div>
+      </section>
+      <section className="text-white">
+        <div className="font-bold my-8">
+          ХОЛБОО БАРИХ
+          <div className="font-medium my-2">Имэйл: info@cody.mn</div>
+          <div className="font-medium my-2">
+            Хаяг: Нью Хоризон оффис, 401 тоот, Улаанбаатар хот
+          </div>
+          <div className="font-medium my-2">Утас: 77778977</div>
+        </div>
+      </section>
       <div>4</div>
     </footer>
   );
