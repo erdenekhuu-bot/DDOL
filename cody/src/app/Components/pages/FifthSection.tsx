b27820a08cc2c8cfd267166ff70c9bc965186542
unknown <hydra2medusa@gmail.com>
2024-10-07 12:34:42 +0800

    'edited'

--- cody/src/app/Components/pages/FifthSection.tsx
+++ cody/src/app/Components/pages/FifthSection.tsx
@@ -13,16 +13,18 @@ const FifthSection = function () {
     setClick(!click);
   };
   return (
-    <section className="flex justify-around flex-wrap bg-gray-200 p-8">
-      {opportunity_list.map((item) => (
-        <Opportunity_item
-          key={item.id}
-          image={item.id}
-          title={item.title}
-          feature={item.feature}
-        />
-      ))}
-      <div className="flex justify-center">
+    <div className="bg-gray-200">
+      <section className="flex justify-around flex-wrap  p-8">
+        {opportunity_list.map((item) => (
+          <Opportunity_item
+            key={item.id}
+            image={item.id}
+            title={item.title}
+            feature={item.feature}
+          />
+        ))}
+      </section>
+      <section className="flex justify-center">
         <Button
           onClick={() => {
             setClick(!click);
@@ -33,8 +35,8 @@ const FifthSection = function () {
           Дэлгэрэнгүй
         </Button>
         {click && <Popup onClose={handlePopupToggle} />}
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
--- cody/src/app/page.tsx
+++ cody/src/app/page.tsx
@@ -1,12 +1,12 @@
 import Stack from "@mui/material/Stack";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
-import FirstSection from "./Components/home/FirstSection";
-import SecondSection from "./Components/home/SecondSection";
-import ThirdSection from "./Components/home/ThirdSection";
-import FourthSection from "./Components/home/FourthSection";
-import FifthSection from "./Components/home/FifthSection";
-import SixthSection from "./Components/home/SixthSection";
+import FirstSection from "./Components/pages/FirstSection";
+import SecondSection from "./Components/pages/SecondSection";
+import ThirdSection from "./Components/pages/ThirdSection";
+import FourthSection from "./Components/pages/FourthSection";
+import FifthSection from "./Components/pages/FifthSection";
+import SixthSection from "./Components/pages/SixthSection";
 
 export default function Home() {
   return (
