fcf32d4b39608690cd11b76273be55341d8c5ace
unknown <hydra2medusa@gmail.com>
2024-10-08 13:57:18 +0800

    'edited'

--- cody/src/app/Components/pages/SixthSection.tsx
+++ cody/src/app/Components/pages/SixthSection.tsx
@@ -16,7 +16,7 @@ const SixthSection = () => {
   };
 
   return (
-    <section className="py-12 my-8 h-[650px] overflow-y-scroll rounded-tl-md rounded-tr-md">
+    <section className="py-12 my-8 overflow-hidden rounded-tl-md rounded-tr-md">
       <div className="flex justify-center">
         <p className="font-bold text-2xl my-8">
           Кодиг ашиглан Та юу бүтээж чадах вэ?
@@ -32,7 +32,7 @@ const SixthSection = () => {
           isActive={activeIndex == index}
         />
       ))} */}
-      <section className="flex">
+      <section className="flex h-[650px] overflow-y-scroll">
         <div className="w-1/3  p-4">
           {slide_events.map((item) => (
             <Event_item_show
