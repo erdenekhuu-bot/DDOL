7f6cc7bb6e4f5be3c7065e89b57621aaede301e4
unknown <hydra2medusa@gmail.com>
2024-10-08 17:40:02 +0800

    'sliding'

--- cody/src/app/Components/pages/SixthSection.tsx
+++ cody/src/app/Components/pages/SixthSection.tsx
@@ -3,6 +3,8 @@ import { useState, createContext } from "react";
 import slide_events from "../list/slide_events";
 import { Event_item_show, Event_item_image } from "../Event_item";
 import Image from "next/image";
+import right from "../../images/right.svg";
+import left from "../../images/left.svg";
 
 export const Context = createContext(0);
 
@@ -11,11 +13,25 @@ const SixthSection = () => {
 
   const handleClick = (index: number) => {
     setPage(index);
+    if (page > arr.length && page < arr.length) {
+      setPage(0);
+    }
+  };
+
+  const navigateRight = function () {
+    setPage(page - 1);
+    if (page > arr.length && page < arr.length) {
+      setPage(0);
+    }
+  };
+
+  const navigateLeft = function () {
+    setPage(page + 1);
   };
 
   const arr: JSX.Element[] = [];
 
-  slide_events.map((item, index) => {
+  slide_events.map((item) => {
     const imageElement = (
       <Image
         key={item.id}
@@ -23,7 +39,7 @@ const SixthSection = () => {
         height={980}
         src={item.image}
         alt=""
-        className="object-cover"
+        className="object-cover rounded-3xl drop-shadow border m-2"
       />
     );
 
@@ -32,7 +48,7 @@ const SixthSection = () => {
 
   return (
     <Context.Provider value={page}>
-      <section className="py-12 my-8 overflow-hidden rounded-tl-md rounded-tr-md">
+      <section className="py-12 my-8 overflow-hidden rounded-tl-md rounded-tr-md z-0">
         <div className="flex justify-center">
           <p className="font-bold text-2xl my-8">
             Кодиг ашиглан Та юу бүтээж чадах вэ?
@@ -49,10 +65,20 @@ const SixthSection = () => {
               />
             ))}
           </div>
-          <div className="w-full flex">
-            <div className="sticky top-0 flex-shrink-0 w-full h-full py-4">
-              {arr[page]}
+          <div className="relative w-full flex">
+            <div
+              onClick={navigateRight}
+              className="absolute right-5 top-72 border bg-white rounded-full p-4 z-10 hover:cursor-pointer"
+            >
+              <Image src={left} width={10} height={10} alt="" />
             </div>
+            <div
+              onClick={navigateLeft}
+              className="absolute left-5 top-72 border bg-white rounded-full p-4 z-10 hover:cursor-pointer"
+            >
+              <Image src={right} width={10} height={10} alt="" />
+            </div>
+            <div className="flex-shrink-0 w-full h-full py-4">{arr[page]}</div>
             {/* {slide_events.map((item, index) => (
               <Event_item_image
                 key={item.id}
