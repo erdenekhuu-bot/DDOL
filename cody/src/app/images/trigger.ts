2e634ea0ec2c510a8a3e1f061b04fde970664567
unknown <hydra2medusa@gmail.com>
2024-10-09 15:40:01 +0800

    'edited'

--- cody/src/app/images/trigger.ts
+++
@@ -1,7 +0,0 @@
-import phone_background from "../../images/phone_template.png";
-import pad_background from "../../images/pad_template.png";
-
-export const trigger = {
-  phoneBackground: phone_background,
-  padBackground: pad_background,
-};
