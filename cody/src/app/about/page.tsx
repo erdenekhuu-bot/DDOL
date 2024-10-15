e0f093b7fb745d9fa6cfefc80567dadf0b2d8a8f
unknown <hydra2medusa@gmail.com>
2024-10-15 12:28:45 +0800

    addnavbars

--- cody/src/app/about/page.tsx
+++ cody/src/app/about/page.tsx
@@ -1,6 +1,6 @@
 const About = function () {
   return (
-    <section className="h-[800px]">
+    <section className="h-[800px] bg-cover bg-center">
       <div>About</div>
     </section>
   );
--- cody/src/app/career/page.tsx
+++ cody/src/app/career/page.tsx
@@ -1,7 +1,17 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import career from "../../app/images/career.jpg";
+
 const Career = function () {
   return (
-    <section className="h-[800px]">
-      <div>Career</div>
+    <section
+      className="h-[800px] bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${career.src})`,
+      }}
+    >
+      <div>About</div>
     </section>
   );
 };
