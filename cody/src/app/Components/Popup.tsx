a387a8f5579a86d17e9e62d3e281f43b052f677c
unknown <hydra2medusa@gmail.com>
2024-10-15 12:55:00 +0800

    editing

--- cody/src/app/Components/Popup.tsx
+++ cody/src/app/Components/Popup.tsx
@@ -111,7 +111,7 @@ const Popupheader = function ({
 }) {
   return (
     <section
-      className={`fixed z-0 top-20 right-0 left-0 bg-white transition-all duration-300`}
+      className={`fixed z-0 top-16 right-0 left-0 bg-white transition-all duration-300 md:hidden`}
     >
       <div
         className={`overflow-hidden transition-all duration-300 ${
