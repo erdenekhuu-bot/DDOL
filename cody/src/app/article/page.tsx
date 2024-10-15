97b61c8de82bafe18c2f8932f84a1d9b66cd07e0
unknown <hydra2medusa@gmail.com>
2024-10-15 12:21:14 +0800

    editing

--- cody/src/app/article/page.tsx
+++ cody/src/app/article/page.tsx
@@ -1,6 +1,16 @@
+"use client";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import ga4 from "../../app/images/ga4.jpg";
+
 const Article = function () {
   return (
-    <section className="h-[800px]">
+    <section
+      className="h-[800px] bg-cover bg-center"
+      style={{
+        backgroundImage: `url(${ga4.src})`,
+      }}
+    >
       <div>Article</div>
     </section>
   );
