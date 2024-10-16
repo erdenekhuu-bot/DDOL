90ee688c22d00bcfa1d9cdede9762f8e00313ebd
unknown <hydra2medusa@gmail.com>
2024-10-16 13:40:12 +0800

    'final'

--- fullcody/cody/src/app/components/pages/PopUp.tsx
+++ fullcody/cody/src/app/components/pages/PopUp.tsx
@@ -1,5 +1,11 @@
-import { navList } from "@/app/json/objects";
+import { mobilenavList } from "@/app/json/objects";
 import { navlink } from "@/app/types/type";
+import x from "../../images/x.svg";
+import face from "../../images/face.svg";
+import inn from "../../images/in.svg";
+import insta from "../../images/insta.svg";
+import Image from "next/image";
+import cody from "../../images/cody.svg";
 
 export const Nav = function ({
   call,
@@ -17,7 +23,49 @@ export const Nav = function ({
           call ? `max-h-screen` : `max-h-0`
         }`}
       >
-        <div></div>
+        {mobilenavList.map((items) => (
+          <div className="p-2 border">
+            <p className="text-sm">{items.title}</p>
+          </div>
+        ))}
+        <div className="my-4">
+          <div className="flex p-4 items-center justify-center">
+            <Image
+              src={insta}
+              alt=""
+              width={800}
+              height={800}
+              className="w-8 mx-2"
+            />
+            <Image
+              src={face}
+              alt=""
+              width={800}
+              height={800}
+              className="w-8 mx-2"
+            />
+            <Image
+              src={x}
+              alt=""
+              width={800}
+              height={800}
+              className="w-8 mx-2"
+            />
+            <Image
+              src={inn}
+              alt=""
+              width={800}
+              height={800}
+              className="w-8 mx-2"
+            />
+          </div>
+        </div>
+        <div className="my-4 flex justify-center">
+          ©2024 Cody Inc. All rights reserved.
+        </div>
+        <div className="mx-auto w-1/2 my-4">
+          <Image src={cody} alt="" width={800} height={800} className="w-32" />
+        </div>
       </div>
     </section>
   );
--- fullcody/cody/src/app/json/objects.ts
+++ fullcody/cody/src/app/json/objects.ts
@@ -32,3 +32,41 @@ export const navList: navlink = [
     url: "/career",
   },
 ];
+
+export const mobilenavList: navlink = [
+  {
+    id: 1,
+    title: "Коди Платформ",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "Шийдэл",
+    url: "/",
+  },
+  {
+    id: 3,
+    title: "Боломжууд",
+    url: "/",
+  },
+  {
+    id: 4,
+    title: "Зөвөлгөө",
+    url: "/article",
+  },
+  {
+    id: 5,
+    title: "Бидний тухай",
+    url: "/about",
+  },
+  {
+    id: 6,
+    title: "Ажлын байр",
+    url: "/career",
+  },
+  {
+    id: 7,
+    title: "Холбоо барих",
+    url: "/",
+  },
+];
