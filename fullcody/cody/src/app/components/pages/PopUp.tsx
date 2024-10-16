ed363bba27234050518d936f7188634c990c19d9
unknown <hydra2medusa@gmail.com>
2024-10-16 13:16:55 +0800

    'final'

--- fullcody/cody/src/app/components/pages/PopUp.tsx
+++ fullcody/cody/src/app/components/pages/PopUp.tsx
@@ -1,3 +1,6 @@
+import { navList } from "@/app/json/objects";
+import { navlink } from "@/app/types/type";
+
 export const Nav = function ({
   call,
   click,
@@ -14,16 +17,7 @@ export const Nav = function ({
           call ? `max-h-screen` : `max-h-0`
         }`}
       >
-        <p>1</p>
-        <p>2</p>
-        <p>3</p>
-        <p>4</p>
-        <p>5</p>
-        <p>6</p>
-        <p>7</p>
-        <p>8</p>
-        <p>9</p>
-        <p>10</p>
+        <div></div>
       </div>
     </section>
   );
