c4c13e957def968a299712b79632857bb67994a7
unknown <hydra2medusa@gmail.com>
2024-10-18 13:39:17 +0800

    'final'

--- fullcody/cody/src/app/career/page.tsx
+++ fullcody/cody/src/app/career/page.tsx
@@ -3,13 +3,20 @@ import career from "../images/career.jpg";
 
 const Career = function () {
   return (
-    <section
-      className="h-[800px] bg-cover bg-center bg-gradient-to-bl "
-      style={{
-        backgroundImage: `url(${career.src})`,
-      }}
-    >
-      <div>About</div>
+    <section className="h-[800px]">
+      <div
+        className="relative h-[480px] bg-cover bg-center"
+        style={{
+          backgroundImage: `url(${career.src})`,
+        }}
+      >
+        <p className="absolute bottom-24 font-bold text-white text-xl">
+          Бид авьяаслаг, чөлөөт сэтгэхүйтэй, бүтээлч, хөдөлмөрч залуустай
+          хамтран ажиллахдаа үргэлж нээлттэй байх болно.
+        </p>
+      </div>
+
+      <div className="z-10 ">About</div>
     </section>
   );
 };
