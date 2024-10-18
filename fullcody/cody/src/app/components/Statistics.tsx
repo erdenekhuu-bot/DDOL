ce93176be90ec23ebe0f3ace892bff1432ed04fa
unknown <hydra2medusa@gmail.com>
2024-10-18 15:00:31 +0800

    'final'

---
+++ fullcody/cody/src/app/components/Statistics.tsx
@@ -0,0 +1,10 @@
+import Statistic from "antd/es/statistic/Statistic";
+
+export const Statistics = function () {
+  return (
+    <section className="absolute top-1/2 w-full">
+      <p className="font-bold text-center text-white py-6">Статистик</p>
+      <div></div>
+    </section>
+  );
+};
