99109980e063699707cafd96fbe2774ff28321de
unknown <hydra2medusa@gmail.com>
2024-10-22 10:41:42 +0800

    'final'

--- fullcody/cody/src/app/components/Services.tsx
+++ fullcody/cody/src/app/components/Services.tsx
@@ -26,18 +26,19 @@ export const Services = function () {
 
   console.log(getService);
 
+  let counter = 1;
   return (
     <section className="bg-gray-50 py-8 z-0">
-      <p className="font-bold text-center py-6 text-3xl">Бидний тухай</p>
+      <p className="font-bold text-center py-6 text-3xl">Үйлчилгээний төрөл</p>
       <div className="flex flex-wrap justify-evenly">
         {getService.map((items: any) => (
           <div
             key={items.id}
             className="relative w-[200px] m-4 h-[200px] rounded-lg"
           >
-            <p className="font-bold text-[60px] text-purple-700">{items.id}</p>
+            <p className="font-bold text-[60px] text-purple-700">{0}</p>
             <p className="absolute font-bold text-[60px] top-0 left-9 z-20 text-purple-700">
-              {items.id + "."}
+              {counter++ + "."}
             </p>
             <div className="absolute bg-white rounded-xl w-[90%] h-[90%] top-10 left-8 z-10 drop-shadow-xl flex items-center">
               <b className="text-center">{items.name}</b>
